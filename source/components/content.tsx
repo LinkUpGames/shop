0ff5f55da1f71083598ef7c6fa9226609f6a5008
Anonymous <mjcevallos007@gmail.com>
2025-07-30 18:46:49 -0400

    coool

---
+++ source/components/content.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Box} from 'ink';
+
+type Props = {};
+
+/**
+ * The content to display
+ */
+const content = ({}: Props) => {
+	return <Box height={'100%'}></Box>;
+};
+
+export default content;
---
+++ source/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import {Box} from 'ink';
+import React from 'react';
+
+/**
+ * Sidebar
+ */
+const sidebar = () => {
+	return <Box height={'100%'}></Box>;
+};
+
+export default sidebar;
